e9ea6dbe94834b73a91ef5a36d92424b616151a8
Ashok Singh <ashok889@gmail.com>
2020-06-01 19:41:25 +0530

    commited code

---
+++ CustumWorkflow/CustomWF/CustomWFAccount.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.ObjectModel;
+
+namespace CustomWF
+{
+    public class CustomWFAccount : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            //Create the tracing service
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            //Create the context
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            tracingService.Trace("Creating Account");
+
+            Entity ac = new Entity("account");
+            ac["name"] = "Ashok Singh";
+            Guid entityId = service.Create(ac);
+
+            tracingService.Trace("Account created with Id {0}", entityId.ToString());
+
+        }
+    }
+}
